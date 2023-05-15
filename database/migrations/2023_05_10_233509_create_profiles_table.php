e12c667ae5ea640351b56d50b0e7eb5ed017365e
ZINEEDDINE LOURIGA <zineeddinelouriga2@gmail.com>
2023-05-15 18:16:18 +0100

    modifs sur la table profiles

--- database/migrations/2023_05_10_233509_create_profiles_table.php
+++ database/migrations/2023_05_10_233509_create_profiles_table.php
@@ -16,6 +16,7 @@ return new class extends Migration
         Schema::create('profiles', function (Blueprint $table) {
             $table->id();
             $table->string('name',80);
+            $table->string('genre',20);
             $table->string('email',70)->unique();
             $table->string('password',60);
             $table->text('bio');
