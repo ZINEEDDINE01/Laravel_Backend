542f6b65b7e7e3e505d54f28510527d47d97b96e
ZINEEDDINE LOURIGA <zineeddinelouriga2@gmail.com>
2023-05-17 14:03:32 +0100

    Installation de la partie frontend avec vuejs

--- resources/views/hello.blade.php
+++ resources/views/hello.blade.php
@@ -1,6 +1,22 @@
-<h3>
-Hello 
-@php
-echo $nom;    
-@endphp
-</h3>
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Laravel - Vue 3</title>
+</head>
+<body>
+
+    <!-- L'élément HTML d'identifiant "app" -->
+    <div id="app" >
+        <h1>Bonjour</h1>
+        <h1>Bonsoir</h1>
+    </div>
+
+    <!-- On importe le fichier « /public/js/app.js » -->
+    <script type="text/javascript   " src="{{ asset('js/app.js') }}" >
+    
+    </script>
+
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Log;
+use App\Http\Controllers\HomeController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +18,5 @@ Route::get('/', function () {
     Log::channel('slackInfo')->info('Page Welcome');
     return view('welcome');
 });
+
+Route::get('/hello',[HomeController::class,'index']);
